3b72085591fd1f8ab4b17ea2acf33299977a81e8
Shaqeeb Momen <shaqeebmomen@users.noreply.github.com>
2020-07-31 20:17:07 -0400

    Console History implemented

--- components/dash-logger.js
+++ components/dash-logger.js
@@ -1,4 +1,4 @@
-import nfSocket  from '../util_modules/websocket.js'
+import nfSocket from '../util_modules/websocket.js'
 
 const template = document.createElement('template');
 
@@ -6,6 +6,9 @@ const template = document.createElement('template');
 class DashLogger extends HTMLElement {
     constructor() {
         super();
+        this.commands = [];
+        this.commandIndex = 0;
+        this.commandLengthMax = 10;
         fetch("./components/dash-logger.html")
             .then((response) => {
                 return response.text();
@@ -18,14 +21,42 @@ class DashLogger extends HTMLElement {
                 this.commandSender = this.shadowRoot.querySelector('#commandSender');
 
                 // Event Listeners
-
                 this.commandSender.addEventListener("keyup", (event) => {
-                    if (event.code === 'Enter') {
+                    if (event.code === "Enter") {
                         nfSocket.sendText(this.commandSender.value);
-                        this.commandSender.value = "";
-                    }
+                        if (this.commands.length === this.commandLengthMax) {
+                            this.commands.pop();
+                        }
 
+                        this.commands.unshift(this.commandSender.value);
+                        this.commandSender.value = "";
+                        this.commandIndex = -1;
+                        console.log(this.commands);
+                    }
+                    if (event.code === "ArrowUp") {
+
+                        if (this.commandIndex < this.commands.length - 1) {
+                            this.commandIndex++;
+                            this.commandSender.value = this.commands[this.commandIndex];
+                        }
+
+
+                    }
+                    else if (event.code === "ArrowDown") {
+                        if (this.commandIndex > 0) {
+                            this.commandIndex--;
+                            this.commandSender.value = this.commands[this.commandIndex];
+                        }
+                        else if(this.commandIndex == 0) {
+                            this.commandIndex--;
+                            this.commandSender.value = "";
+                            
+                        }
+
+                    }
+                    console.log(this.commandIndex);
                 });
+
 
                 this.logger.addEventListener("socketupdate", (event) => {
                     let date = new Date();
